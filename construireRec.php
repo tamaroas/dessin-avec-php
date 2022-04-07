4bd0cb77ea3f99729ca246a49ad696bf94991df6
Tahago Rodrigue <80415054+tamaroas@users.noreply.github.com>
2022-04-07 07:01:27 +0100

    Add files via upload

--- construireRec.php
+++ construireRec.php
@@ -1,12 +1,13 @@
 <?php 
 //require_once "Cercle.php";
 
-$rayon= $_POST['r'];
+$long= $_POST['long'];
+$larg= $_POST['larg'];
 $couleur= $_POST['color'];
 
 header("content-type:image/png");
-$x=400;
-$y=300;
+$x=1000;
+$y=800;
 $img = imagecreatetruecolor($x,$y);
 $bleu=imagecolorallocate($img, 0, 0, 200);
 $rouge=imagecolorallocate($img, 255, 0, 42);
@@ -14,26 +15,26 @@ $verte=imagecolorallocate($img, 0, 255, 42);
 $blanc=imagecolorallocate($img, 255, 255, 255);
 $jaune=imagecolorallocate($img, 255, 255, 0);
 
-
 if ($couleur=="bleu") {
-	imagefilledellipse($img, 2*$x/4, $y/2, $rayon/2-25, $rayon/2-25, $bleu);
+	imagefilledrectangle($img,25,25,37.7953*$long-25,37.7953*$larg-25,$bleu);
     imagepng($img);
 }
 elseif ($couleur=="white") {
-	imagefilledellipse($img, 2*$x/4, $y/2, $rayon/2-25, $rayon/2-25, $blanc);
+    imagefilledrectangle($img,25,25,37.7953*$long-25,37.7953*$larg-25,$blanc);
     imagepng($img);
 }
 elseif ($couleur=="green") {
-	imagefilledellipse($img, 2*$x/4, $y/2, $rayon/2-25, $rayon/2-25, $verte);
+	imagefilledrectangle($img,25,25,37.7953*$long-25,37.7953*$larg-25,$verte);
     imagepng($img);
 }
 elseif ($couleur=="yellow") {
-	imagefilledellipse($img, 2*$x/4, $y/2, $rayon/2-25, $rayon/2-25, $jaune);
+	imagefilledrectangle($img,25,25,37.7953*$long-25,37.7953*$larg-25,$jaune);
     imagepng($img);
 }
 elseif ($couleur=="red") {
-	imagefilledellipse($img, 2*$x/4, $y/2, $rayon/2-25, $rayon/2-25, $rouge);
+	imagefilledrectangle($img,25,25,37.7953*$long-25,37.7953*$larg-25,$rouge);
     imagepng($img);
 }
+//imagerectangle($img,25,25,$x/2-25,$y/2-25,$bleu);
 
 ?>
